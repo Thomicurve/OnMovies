a71466ff7edaf53d30297a23f055f9d46aef6561
Thomicurve <thomigrax@gmail.com>
2021-04-23 15:07:52 -0300

    fix 1

--- src/components/ApiMovies.js
+++ src/components/ApiMovies.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+
+const apiKey = '44f76584';
+
+
 const getMovies = async (movieName)=>{
     if(movieName.trim('').length !== 0){
-        const result = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_APIKEY}&s=${movieName}`);
+        const result = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${movieName}`);
         return result.data.Search;
     }
     
