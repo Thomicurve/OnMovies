b4094243c73ac089dd7e2e3f779863da22a1b5a0
Thomicurve <thomigrax@gmail.com>
2021-06-02 14:04:51 -0300

    change http to https link api

--- src/components/ApiMovies.js
+++ src/components/ApiMovies.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
 
-const apiKey = '44f76584';
-
-
 const getMovies = async (movieName)=>{
     if(movieName.trim('').length !== 0){
-        const result = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${movieName}`);
+        const result = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=${process.env.APIKEY}&s=${movieName}`);
         return result.data.Search;
     }
     
