520425398637dd126a2208f3261b0c1814d22fb1
Thomicurve <thomigrax@gmail.com>
2021-06-02 14:51:42 -0300

    fixing apikey

--- src/components/ApiMovies.js
+++ src/components/ApiMovies.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const getMovies = async (movieName)=>{
     if(movieName.trim('').length !== 0){
-        const result = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=${process.env.APIKEY}&s=${movieName}`);
+        const result = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=44f76584&s=${movieName}`);
         return result.data.Search;
     }
     
